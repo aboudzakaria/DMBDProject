ae186c5a7dafa7febe857e26d1520efbfa581f20
Aboud <aboud.zakaria@gmail.com>
2018-12-21 14:35:31 +0100

    comment and clean code

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,6 +1,6 @@
 """ preprocess xml files to txt. """
 
-import os, re, string, pickle
+import os, re, string
 from lxml import etree
 from nltk import word_tokenize
 from nltk.corpus import stopwords, wordnet
@@ -39,78 +39,88 @@ porter = PorterStemmer()
 token_vector = set([])
 
 for filename in sorted(os.listdir(XML_DIR)):
-    if filename.lower().endswith(".xml"):
-        if n < 0 or i < n:
-            i += 1
-        else:
+    # skip not .xml files
+    if not filename.lower().endswith(".xml"):
+        continue
+
+    # process limits
+    if n < 0 or i < n:
+        i += 1
+    else:
+        break
+
+    tknfilename = filename.replace(".xml", ".tkn")
+
+    # already processed?
+    if os.path.isfile(os.path.join(TKN_DIR, tknfilename)) and not FORCE_PROCESS:
+        continue
+
+    try:
+        # remove xml tags
+        tree = etree.parse(os.path.join(os.path.join(XML_DIR, filename)))
+        raw_text = etree.tostring(tree, encoding='utf8', method='text').decode('ascii')
+
+        # check if there's no text
+        if not raw_text:
+            no_text.append(filename)
+            if not FORCE_NO_TEXT:
+                print("SKIPPING: " + filename)
+                continue
+
+        # manual fixes
+        raw_text = raw_text.replace('-', ' ') # dashes!
+        raw_text = raw_text.replace('/', ' ') # slashes!
+        raw_text = raw_text.replace('+', ' ') # pluses!
+        raw_text = raw_text.lower()
+
+        # nltk tokenize words
+        tokens = word_tokenize(raw_text)
+
+        # process tokens
+        clean_tokens = []
+        for w in tokens:
+            # filter punctuations with extra check!
+            for p in PUNCTUATIONS:
+                w = w.replace(p, '')
+            # filter stopwords
+            if w in STOPWORDS:
+                continue
+            # replace numbers into the word 'numero'
+            if bool(re.match(HAS_NUMBERS, w)):
+                w = "numero"
+            # try finding a synonym which is already in the vector!
+            try:
+                synonyms = [porter.stem(s.lower()) for s in wordnet.synsets(w)[0].lemma_names()]
+                for s in synonyms:
+                    if s in token_vector and w != s:
+                        print(w + ' -> ' + s)
+                        w = s
+            except:
+                pass
+            # keep words with length > 2
+            if len(w) < 3:
+                continue
+            # stem the word
+            w = porter.stem(w)
+            # finally insert the word
+            clean_tokens.append(w)
+            # add once to the vector set
+            token_vector.add(w)
+
+        # write data
+        with open(os.path.join(TKN_DIR, tknfilename), 'w') as tknfile:
+            tknfile.write('\n'.join(clean_tokens))
+
+    except Exception as err:
+        print("Exception: {0}".format(err))
+        if BREAK_ON_ERROR:
             break
-        tknfilename = filename.replace(".xml", ".tkn")
-        if os.path.isfile(os.path.join(TKN_DIR, tknfilename)) and not FORCE_PROCESS:
-            continue
 
-        try:
-            # remove xml tags
-            tree = etree.parse(os.path.join(os.path.join(XML_DIR, filename)))
-            raw_text = etree.tostring(tree, encoding='utf8', method='text').decode('ascii')
 
-            # check if there's no text
-            if not raw_text:
-                no_text.append(filename)
-                if not FORCE_NO_TEXT:
-                    print("SKIPPING: " + filename)
-                    continue
+with open('no_text.csv', 'w') as no_textfile:
+    no_textfile.write('\n'.join(sorted(no_text)))
 
-            # manual fixes
-            raw_text = raw_text.replace('-', ' ') # dashes!
-            raw_text = raw_text.replace('/', ' ') # slashes!
-            raw_text = raw_text.replace('+', ' ') # pluses!
-            raw_text = raw_text.lower()
-
-            # nltk tokenize words
-            tokens = word_tokenize(raw_text)
-
-            # process tokens
-            clean_tokens = []
-            for w in tokens:
-                # filter punctuations with extra check!
-                for p in PUNCTUATIONS:
-                    w = w.replace(p, '')
-                # filter stopwords
-                if w in STOPWORDS:
-                    continue
-                # replace numbers into the word 'number'
-                if bool(re.match(HAS_NUMBERS, w)):
-                    w = "number"
-                # try finding a synonym which is already in the vector!
-                try:
-                    synonyms = [porter.stem(s.lower()) for s in wordnet.synsets(w)[0].lemma_names()]
-                    for s in synonyms:
-                        if s in token_vector and w != s:
-                            print(w + ' -> ' + s)
-                            w = s
-                except:
-                    pass
-                # keep words with length > 2
-                if len(w) < 3:
-                    continue
-                # stem the word
-                w = porter.stem(w)
-                # finally insert the word
-                clean_tokens.append(w)
-                # add once to the vector set
-                token_vector.add(w)
-
-            # pickle data
-            with open(os.path.join(TKN_DIR, tknfilename), 'w') as tknfile:
-                tknfile.write('\n'.join(clean_tokens))
-
-        except Exception as err:
-            print("Exception: {0}".format(err))
-            if BREAK_ON_ERROR:
-                break
-
-pickle.dump(no_text, open('no_text.pkl', 'wb'))
-pickle.dump(token_vector, open('token_vector.pkl', 'wb'))
 with open('token_vector.tkn', 'w') as vecfile:
     vecfile.write('\n'.join(sorted(token_vector)))
-print(len(token_vector))
+
+print("token vector size: " + str(len(token_vector)))
