6120eff662809f769b542062193d6b9e2cad4122
Aboud <aboud.zakaria@gmail.com>
2019-01-27 21:22:40 +0100

    data splitted

--- data_sparse.py
+++ data_sparse.py
@@ -1,3 +1,4 @@
+from sklearn.model_selection import train_test_split
 ''' read data_sparse.csv file int np array '''
 import os
 import numpy as np
@@ -37,3 +38,32 @@ with open('data_sparse.csv', 'r') as sparsefile:
         data_tfidf[docid-1][tokid] = tfidf
 
         l = sparsefile.readline()
+
+no_text = np.genfromtxt('no_text.csv', dtype=str, delimiter='.')[
+    :, 0].astype(int) - 1
+traincsv = np.genfromtxt('train.csv', dtype=str, delimiter=',', skip_header=True)[
+    :, [0, 2]].astype(int)
+traincsv[:, 0] -= 1
+ix = np.invert(np.isin(traincsv[:, 0], no_text))
+trainind = traincsv[ix, 0]
+
+testcsv = np.genfromtxt('test.csv', dtype=str, delimiter=',', skip_header=True)[
+    :, 0].astype(int)
+testcsv[:] -= 1
+ixx = np.invert(np.isin(testcsv[:], no_text))
+testind = testcsv[ixx]
+
+X_b = data_boolean[trainind]
+X_tf = data_tf[trainind]
+X_tfidf = data_tfidf[trainind]
+y = traincsv[ix, 1]
+
+X_train_b, X_validate_b, y_train_b, y_validate_b = train_test_split(X_b, y)
+X_train_tf, X_validate_tf, y_train_tf, y_validate_tf = train_test_split(
+    X_tf, y)
+X_train_tfidf, X_validate_tfidf, y_train_tfidf, y_validate_tfidf = train_test_split(
+    X_tfidf, y)
+
+X_test_b = data_boolean[testind]
+X_test_tf = data_tf[testind]
+X_test_tfidf = data_tfidf[testind]
