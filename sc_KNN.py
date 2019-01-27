f09fd15135d01b5170becd6c6b6900ca361bef7f
Muaz <muaz.sy123@gmail.com>
2019-01-27 22:05:49 +0100

    add sc_Knn

--- sc_KNN.py
+++ sc_KNN.py
@@ -4,6 +4,8 @@ import numpy as np
 from data_sparse import *
 coco = 0
 
+from sklearn.neighbors import KNeighborsClassifier
+
 def U_dis(d1,d2):
     global coco
     if coco % 100 == 0:
@@ -14,20 +16,17 @@ def U_dis(d1,d2):
     L = len(d1)
     return sum( [ (d1[i]-d2[i])**2 for i in range(L)] )
 
-def knn_accuracy(y_train, y_test, dist, ks):
-    sortedind = dist.argsort(axis=0)
-    # majority vote
-    for k in ks:
-        count = 0
-        for i in range(y_test.size):
-            knearest = sortedind[:k, i]
-            y_pred = np.bincount(y_train[knearest]).argmax()
-            if y_pred == y_test[i]:
-                count += 1
-        print("k={0}, accuracy={1}%".format(k, count/y_test.size))
-        sys.stdout.flush()
+def sc_KNN(X_train,X_test,y_train,y_test, k):
+    neigh = KNeighborsClassifier(n_neighbors=k,algorithm='kd_tree' )
+    neigh.fit(X_train, y_train)
+    y_pred = neigh.predict(X_test)
+    """print(y_pred)
+    print(y_test)
+    print(y_pred == y_test)"""
+    return (y_pred == y_test).mean()*100
 
 if __name__ == '__main__':
+    __debug = True
     ks = [i for i in range(1, 21)]
     
     #get data here
@@ -35,13 +34,17 @@ if __name__ == '__main__':
     X_test = X_validate_tf
     y_train = y_train_tf
     y_test = y_validate_tf
-    
+    if __debug:
+        ks = [i for i in range(1, 9)]
+        X_train = X_train[:100,:]
+        X_test = X_test[:10,:]
+        y_train = y_train[:100]
+        y_test = y_test[:10]
+       
     print("X_train shape", X_train.shape)
     print("X_test shape", X_test.shape)
     print("y_train shape", y_train.shape)
     print("y_test shape", y_test.shape)
-        
-    train_test_dis = [ [ U_dis(X_train[i],X_test[j]) for j in range(y_test.size) ] for i in range(y_train.size) ]
-    print('finished calc distance')
-    knn_accuracy(y_train, y_test, train_test_dis, ks)
     
+    for k in ks:
+        print(sc_KNN(X_train,X_test,y_train,y_test,k))
