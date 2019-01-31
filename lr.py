61ce98644ac36a88e93096459eb195782f10de6c
Karthik Bhaskar <b.krtik@hotmail.com>
2019-01-31 17:01:03 +0100

    add confusion matrix and log results

--- lr.py
+++ lr.py
@@ -20,18 +20,20 @@ if __name__ == '__main__':
         X_test = X_test[:100,:]
         y_train = y_train[:100]
         y_test = y_test[:100]
-    
+    print("\nUsing Logistic Regression .. \n")
     print("X_train shape", X_train.shape)
     print("X_test shape", X_test.shape)
     print("y_train shape", y_train.shape)
     print("y_test shape", y_test.shape)
-    
+    print("\n")
     logreg = LogisticRegression()
     logreg.fit(X_train,y_train)
     y_pred = logreg.predict(X_test)
 
-print('Accuracy of logistic regression classifier: {:.3f}'.format(logreg.score(X_test, y_test)))
+print('Accuracy: {:.3f}'.format(logreg.score(X_test, y_test)))
 f1 = f1_score(y_test, y_pred) , (y_pred == y_test).mean()
-print("F1 score is ",f1[0])
-#print(f1[1])
+print("\nF1 Score:",f1[0])
+cf_mat = confusion_matrix(y_test, y_pred)
+print("\nConfusion Matrix:")
+print(cf_mat)
 sys.stdout.flush()
